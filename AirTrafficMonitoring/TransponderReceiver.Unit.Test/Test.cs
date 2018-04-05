b85e0494ba4cc29c154ac417d8f49ae16b758d1a
nicho1991 <ladefoged1991@gmail.com>
2018-04-05 10:58:34 +0200

    visualStudio
    Sat op og klar til test!

---
+++ AirTrafficMonitoring/TransponderReceiver.Unit.Test/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TransponderReceiver.Unit.Test
+{
+    [TestFixture]
+    public class TransponderReceiverUnitTest
+    {
+
+        [SetUp]
+        public void setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
