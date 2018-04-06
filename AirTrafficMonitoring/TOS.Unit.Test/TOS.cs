fa8951fc0aaaccdbab4e91a57c09afed4f648ed4
nicho1991 <ladefoged1991@gmail.com>
2018-04-06 11:47:18 +0200

    Test er sat op

---
+++ AirTrafficMonitoring/TOS.Unit.Test/TOS.cs
@@ -0,0 +1,46 @@
+﻿
+using NUnit.Framework;
+
+namespace TOS.Unit.Test
+{
+    [TestFixture]
+    public class TOSTagTest
+    {
+        [Test]
+        public void SendTag6_TagEqualTag()
+        {
+            string data = "ATR423";
+            TOS uut = new TOS(data);
+
+            Assert.That(uut.Tag, Is.EqualTo(data));      
+        }
+
+        [Test]
+        public void SendTag5_TagEqualTONull()
+        {
+            string data = "ATR42";
+            TOS uut = new TOS(data);
+
+            Assert.That(uut.Tag, Is.Null);
+        }
+
+        [Test]
+        public void SendTag7_TagEqualTONull()
+        {
+            string data = "ATR4222";
+            TOS uut = new TOS(data);
+
+            Assert.That(uut.Tag, Is.Null);
+        }
+
+        [Test]
+        public void SendTag6stars_TagEqualTOnull()
+        {
+            string data = "******";
+            TOS uut = new TOS(data);
+
+            Assert.That(uut.Tag, Is.Null);
+        }
+
+    }
+}
---
+++ AirTrafficMonitoring/TOS/TOS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TOS
+{
+    public class TOS
+    {
+        public string Tag { get; set; }
+        public string PosistionX { get; set; }
+        public string PosistionY { get; set; }
+        public string Altitude { get; set; }
+        public string TimeStamp { get; set; }
+
+        //public string HorizontalVelocity { get; set; }
+        //public string CompassDegress { get; set; }
+
+        public TOS(string Receive)
+        {
+
+            string[] ReceivedArray = Seperator(Receive);
+
+            if (TagTest(ReceivedArray[0]))
+            {
+                Tag = ReceivedArray[0];
+            }
+
+        }
+
+        private bool TagTest(string tag)
+        {
+            if (tag.Length == 6 && tag.Any(char.IsLetterOrDigit))
+                return true;
+            else return false;
+        }
+
+        private string[] Seperator(string Seperate)
+        {
+            string pattern = "(;)";
+            string[] result = Regex.Split(Seperate, pattern);
+
+            return result;
+        }
+    }
+}
