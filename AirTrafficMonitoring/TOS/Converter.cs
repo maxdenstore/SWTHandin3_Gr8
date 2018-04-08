132f3bba1e4a83e3182ea21a3f703df345771d9d
Max <maxbj@live.dk>
2018-04-08 14:55:49 +0200

    .
    .

--- AirTrafficMonitoring/TOS/Converter.cs
+++ AirTrafficMonitoring/TOS/Converter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TransponderReceiver;
 
@@ -10,6 +11,7 @@ namespace TOS
 {
     public class Converter
     {
+        private string Tag { get; set; }
         private ITransponderReceiver _transponderReceiver;
 
         public Converter(ITransponderReceiver receiver)
@@ -18,6 +20,22 @@ namespace TOS
 
             _transponderReceiver.TransponderDataReady += transponderReceiverData;
 
+        }
+
+
+        public void convert(string data)
+        {
+            string[] DataSep = Seperator(data);
+            Tag = DataSep[0];
+        }
+
+
+        private string[] Seperator(string Seperate)
+        {
+            string pattern = "(;)";
+            string[] result = Regex.Split(Seperate, pattern);
+
+            return result;
         }
 
         public void transponderReceiverData( object sender, RawTransponderDataEventArgs e) 
