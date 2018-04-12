5f4e75febebb9bc33ecb975a0cf57bcdd4ec08fb
maxdenstore <31626964+maxdenstore@users.noreply.github.com>
2018-04-12 10:51:06 +0200

    Added ATMSystem
    System tilføjet skellet

---
+++ AirTrafficMonitoring/ATMSystem/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOS;
+using TransponderReceiver;
+
+namespace ATMSystem
+{
+    public class airMonitor
+    {
+
+        Converter con = new Converter(TransponderReceiverFactory.CreateTransponderDataReceiver());
+        List<TOS.TOS> monitorList = new List<TOS.TOS>();
+        public airMonitor()
+        {
+
+           con.Converted. 
+        }
+
+        public void getEvent()
+        {
+            while (con.)
+            {
+                
+            }
+        }
+
+
+    }
+}
