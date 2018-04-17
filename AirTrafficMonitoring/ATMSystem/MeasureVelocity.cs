23d5e813cdaf52ecf3d0664c47aec596f8e9151e
nicho1991 <ladefoged1991@gmail.com>
2018-04-17 10:32:48 +0200

    time passed figure out

--- AirTrafficMonitoring/ATMSystem/MeasureVelocity.cs
+++ AirTrafficMonitoring/ATMSystem/MeasureVelocity.cs
@@ -12,6 +12,8 @@ namespace ATMSystem
        public void Measure(TOS.TOS old,TOS.TOS newer)
         {
 
+          double distance = Math.Sqrt(Math.Pow(newer.PosistionX - old.PosistionX, 2) + Math.Pow(newer.PosistionY - old.PosistionY, 2));
+
         } 
 
     }
