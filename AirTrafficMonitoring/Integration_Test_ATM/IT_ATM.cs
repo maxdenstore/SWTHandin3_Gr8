0d22ebc04ec86fc7f375de635c87448de265e760
nicho1991 <ladefoged1991@gmail.com>
2018-04-24 22:30:00 +0200

    Test IT_ATM

--- AirTrafficMonitoring/Integration_Test_ATM/IT_ATM.cs
+++ AirTrafficMonitoring/Integration_Test_ATM/IT_ATM.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using ATMSystem;
+using ATMSystem.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using TOS;
+using TOS.Interfaces;
+using TransponderReceiver;
+using TOS = TOS.TOS;
 
 namespace Integration_Test_ATM
 {
     public class IT_ATM
     {
+        private string testString = "ATR423;39045;12932;14000;20151006213456789";
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        //***********************************************************************TEST OF INPUT*********************************************************************
+        // Test transponderReceive
+        [Test]
+        public void transponderRecieverDataTest()
+        {
+            ITOS stubTos = Substitute.For<ITOS>();
+            AirMonitor air = new AirMonitor();
+            ReceiveTranspond uut = new ReceiveTranspond(TransponderReceiverFactory.CreateTransponderDataReceiver(), air);
+            List<string> test = new List<string>();
+            test.Add(testString);
+
+            uut.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+        }
     }
 }
--- AirTrafficMonitoring/TOS/Interfaces/ITos.cs
+++ AirTrafficMonitoring/TOS/Interfaces/ITos.cs
@@ -12,6 +12,7 @@ namespace TOS.Interfaces
             DateTime TimeStamp { get; }
             double Velocity { get; set; }
             double degress { get; set; }
+            void print();
 
         }
     }
