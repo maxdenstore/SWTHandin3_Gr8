6bfb5cbbb823e89257f130bcdb1b4f953884c666
nicho1991 <ladefoged1991@gmail.com>
2018-04-24 23:02:32 +0200

    IT til et projekt

---
+++ AirTrafficMonitoring/ATM.IT.Test/IT_ATM.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ATMSystem;
+using ATMSystem.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using TOS;
+using TOS.Interfaces;
+using TransponderReceiver;
+using TOS = TOS.TOS;
+
+namespace Integration_Test_ATM
+{
+    public class IT_ATM
+    {
+        private string testString = "ATR423;39045;12932;14000;20151006213456789";
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        //***********************************************************************TEST OF INPUT*********************************************************************
+        // Test transponderReceive
+        [Test]
+        public void transponderRecieverDataTest()
+        {
+            ITOS stubTos = Substitute.For<ITOS>();
+            AirMonitor air = new AirMonitor();
+            ReceiveTranspond uut = new ReceiveTranspond(TransponderReceiverFactory.CreateTransponderDataReceiver(), air);
+            List<string> test = new List<string>();
+            test.Add(testString);
+
+            uut.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+        }
+
+        //***********************************************************************TEST OF INPUT*********************************************************************
+        // Test transponderReceive
+        [Test]
+        public void ATM_Printtest()
+        {
+
+                AirMonitor uut = new AirMonitor();
+
+                ReceiveTranspond receive =
+                    new ReceiveTranspond(TransponderReceiverFactory.CreateTransponderDataReceiver(), uut);
+
+                string Test1xNoConflict = "DTR423;39000;13000;12000;20151006213456789";
+                string Test2xNoConflict = "ATR423;39099;13033;12001;20151006213456789";
+                ISeperation stub = Substitute.For<ISeperation>();
+                List<string> test = new List<string>();
+                test.Add(Test1xNoConflict);
+
+                receive.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+                test.Add(Test2xNoConflict);
+
+                receive.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+                Assert.That(stub.Tag.Equals("ATR423"),Is.True);
+
+        }
+
+
+    }
+}
---
+++ AirTrafficMonitoring/ATM.IT.Test/IT_Test_Receive.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMSystem;
+using ATMSystem.Interfaces;
+using Moq;
+using NSubstitute;
+using NUnit.Framework;
+using TOS;
+using TransponderReceiver;
+
+namespace IT_Test_Receive
+{
+
+    [TestFixture]
+    public class IT_Test_Receive
+    {
+        private string testString = "ATR423;39045;12932;14000;20151006213456789";
+        global::TOS.TOS testTos = new TOS.TOS("ATR423", 39045,12932,14000,
+           new DateTime(2015,10,06,21,34,56,789));
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        //***********************************************************************TEST OF INPUT*********************************************************************
+        // Test transponderReceive
+        [Test]
+        public void transponderRecieverDataTest()
+        {
+            IAirmonitor air = Substitute.For<IAirmonitor>();
+            ReceiveTranspond uut = new ReceiveTranspond(TransponderReceiverFactory.CreateTransponderDataReceiver(), air);
+            List<string> test = new List<string>();
+            test.Add(testString);
+            uut.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+            air.ReceivedWithAnyArgs(1).ReceiveNewTOS(testTos);
+        }
+
+        [Test]
+        public void transponderRecieverDataTest2()
+        {
+            IAirmonitor air = Substitute.For<IAirmonitor>();
+            ReceiveTranspond uut = new ReceiveTranspond(TransponderReceiverFactory.CreateTransponderDataReceiver(), air);
+            List<string> test = new List<string>();
+            test.Add(testString);
+            test.Add(testString);
+            uut.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+            air.ReceivedWithAnyArgs(2).ReceiveNewTOS(testTos);
+        }
+
+        [Test]
+        public void transponderRecieverDataTest_noSendNoReceive()
+        {
+            IAirmonitor air = Substitute.For<IAirmonitor>();
+            ReceiveTranspond uut = new ReceiveTranspond(TransponderReceiverFactory.CreateTransponderDataReceiver(), air);
+            List<string> test = new List<string>();
+
+            uut.transponderReceiverData(this, new RawTransponderDataEventArgs(test));
+
+            air.DidNotReceiveWithAnyArgs().ReceiveNewTOS(testTos);
+
+        }
+    }
+}
