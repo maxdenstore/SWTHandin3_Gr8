a1a6acca15a0f305ba82d2aa175e25948f013f41
nicho1991 <ladefoged1991@gmail.com>
2018-05-12 17:09:48 +0200

    oprydning

--- AirTrafficMonitoring/ATMSystem/Interfaces/IDetectSepartation.cs
+++ AirTrafficMonitoring/ATMSystem/Interfaces/IDetectSepartation.cs
@@ -5,8 +5,7 @@ namespace ATMSystem.Interfaces
     public interface IDetectSepartation
     {
         void detect(ITranspondObject a, ITranspondObject b);
-        void handleConflict(ITranspondObject a, ITranspondObject b);
-        void handleNoConflict(ITranspondObject a, ITranspondObject b);
+
 
         void printSeparations();
 
