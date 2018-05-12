de2ed3c31690457a80e6282f58c2a6578d22c905
Max <maxbj@live.dk>
2018-05-12 14:48:47 +0200

    DetectSepUnittest

--- AirTrafficMonitoring/ATMSystem.unit.tests/Tests/DetectSeparation.Uni.Test.cs
+++ AirTrafficMonitoring/ATMSystem.unit.tests/Tests/DetectSeparation.Uni.Test.cs
@@ -28,12 +28,13 @@ namespace ATMSystem.unit.tests.Tests
             _uut = new DetectSepartation(_out);
         }
 
+        //Exptected conflict, Y,X, within 300 & Altitude within 5000
 
         [Test]
-        public void Detect()
+        public void Detect_Conflict_Exptected_True()
         {
-            _fakTranspondObjectA.PosistionX = 1;
-            _fakTranspondObjectA.PosistionY = 1;
+            _fakTranspondObjectA.PosistionX = 100;
+            _fakTranspondObjectA.PosistionY = 111;
             _fakTranspondObjectA.Altitude = 200;
 
 
@@ -49,10 +50,70 @@ namespace ATMSystem.unit.tests.Tests
             Assert.That(_uut.Conflict, Is.True);
         }
 
+        //Exptected no conflict, Y,X, within 300 & Altitude not within 5000
+
         [Test]
-        public void tagOfSep()
+        public void Detect_Conflict_Exptected_False()
         {
-       //     Assert.That(_sep.Tag, Is.EqualTo(_tag));
+            _fakTranspondObjectA.PosistionX = 111;
+            _fakTranspondObjectA.PosistionY = 1111;
+            _fakTranspondObjectA.Altitude = 10000;
+
+
+
+            _fakTranspondObjectB.PosistionY = 300;
+            _fakTranspondObjectB.PosistionX = 120;
+            _fakTranspondObjectB.Altitude = 230;
+
+
+
+            _uut.detect(_fakTranspondObjectA, _fakTranspondObjectB);
+
+            Assert.That(_uut.Conflict, Is.False);
+        }
+
+        //Exptected no conflict, Y not within 300, X Is within 300 & Altitude within 5000
+
+        [Test]
+        public void Detect_ConflictY_Exptected_False()
+        {
+            _fakTranspondObjectA.PosistionX = 1;
+            _fakTranspondObjectA.PosistionY = 300;
+            _fakTranspondObjectA.Altitude = 1000;
+
+
+
+            _fakTranspondObjectB.PosistionY = 800;
+            _fakTranspondObjectB.PosistionX = 20;
+            _fakTranspondObjectB.Altitude = 230;
+
+
+
+            _uut.detect(_fakTranspondObjectA, _fakTranspondObjectB);
+
+            Assert.That(_uut.Conflict, Is.False);
+        }
+
+        //Exptected no conflict, Y within 300, X Is not within 300 & Altitude within 5000
+
+        [Test]
+        public void Detect_ConflictX_Exptected_False()
+        {
+            _fakTranspondObjectA.PosistionX = 100;
+            _fakTranspondObjectA.PosistionY = 300;
+            _fakTranspondObjectA.Altitude = 500;
+
+
+
+            _fakTranspondObjectB.PosistionY = 200;
+            _fakTranspondObjectB.PosistionX = 600;
+            _fakTranspondObjectB.Altitude = 500;
+
+
+
+            _uut.detect(_fakTranspondObjectA, _fakTranspondObjectB);
+
+            Assert.That(_uut.Conflict, Is.False);
         }
     }
 }
